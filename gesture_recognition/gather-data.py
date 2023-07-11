ef054e1b1b96f34daa4ddf3acdbddc69e1d808c8
vairasza <vairasza@mailbox.org>
2023-07-11 11:11:20 +0200

    add multiple recordings per run

--- gesture_recognition/gather-data.py
+++ gesture_recognition/gather-data.py
@@ -15,7 +15,7 @@ class CapabilityValues(TypedDict):
 
 class Input:
 
-  def __init__(self, port: int, capabilities: list[str], button_key: str) -> None:
+  def __init__(self, port: int, capabilities: list[str], toggle_button: str, terminate_button: str) -> None:
     self._port = port
     self._sensor = SensorUDP(self._port)
     
@@ -23,23 +23,32 @@ class Input:
     self._state: dict[str, CapabilityValues] = {}
 
     #handle button_1 events separately so that we do not have to split it from the rest of the sensor data later
-    self._button_key = button_key
-    self._button_pressed = False
+    self._toggle_button = toggle_button
+    self._terminate_button = terminate_button
+    self.__toggle_button_pressed = False
   
-  def get_button_state(self) -> bool:
+  def get_button_1(self) -> bool:
     '''
-      returns boolean value that indicates if button_1 from M5Stack was pressed. M5Stack should only return `True` once in the application cylce as the start is depending on the button press and the stop is depending on a time function (duration).
+      returns boolean value that indicates if button_1 from M5Stack was pressed. if M5Stack returns a value that can not be type cast to boolean, it returns `False`. As M5Stack returns `True` as long as a button is held, therefore button_1_pressed was introduced. all `True` values except the first and until the button is released are turned to `False`. Therefore get_button_1 only returns `True` once for the switch from not pressed to pressed. button_1 is used to quit the game and only one `True` value is required for that.
     '''
-    pressed = self._sensor.get_value(self._button_key)
+    pressed = bool(self._sensor.get_value(self._toggle_button))
 
-    if pressed and pressed == 1 and not self._button_pressed:
-      self._button_pressed = True
+    if pressed and not self.__toggle_button_pressed:
+      self.__toggle_button_pressed = True
 
       return True
+    
+    elif not pressed and self.__toggle_button_pressed:
+      self.__toggle_button_pressed = False
 
     return False
+
+  def get_button_2(self) -> bool:
+    pressed = bool(self._sensor.get_value(self._terminate_button))
+
+    return pressed
   
-  def get_state(self) -> dict[str, CapabilityValues]:
+  def get_acceleration(self) -> dict[str, CapabilityValues]:
     for capability in self._capabilities:
       data = self._sensor.get_value(capability)
 
@@ -56,17 +65,16 @@ class Application:
 
   PORT = 5700
   CURR_DIR = os.path.dirname(__file__)
-  BUTTON_KEY = "button_1"
+  TOGGLE_BUTTON = "button_1"
+  TERMINATE_BUTTON = "button_2"
   CSV_HEADER = "id,time_stamp,gesture,accelerometer_x,accelerometer_y,accelerometer_z\n"
   CAPABILITIES = ["accelerometer"]
 
-  def __init__(self, gesture: Gesture, duration: int, pps: int, wait: int, file_location: str) -> None:
-    self._input = Input(self.PORT, self.CAPABILITIES, self.BUTTON_KEY)
+  def __init__(self, gesture: Gesture, pps: int, file_location: str) -> None:
+    self._input = Input(self.PORT, self.CAPABILITIES, self.TOGGLE_BUTTON, self.TERMINATE_BUTTON)
 
     self._gesture = gesture
-    self._duration =  duration
     self._pps = 1 / pps #polls per second; callback mode averages around 77 per second; setting default to 50 to avoid too many double measurements;
-    self._wait = wait
     self._file_location = file_location
 
     self._running = True
@@ -86,27 +94,29 @@ class Application:
     return path
 
   def run(self) -> None:
-    start_time = None
-    print("waiting for button_1 press to start recording...\n")
-
     while self._running:
-      if self._input.get_button_state():
-        print(f"* * * recording starts in {self._wait} seconds. * * *\n")
-        time.sleep(self._wait)
+      if self._input.get_button_2():
+        print("terminating app...")
+        self._input.terminate_sensor()
+        sys.exit()
+
+      button_1_pressed = self._input.get_button_1()
+
+      if not self._recording and button_1_pressed:
+        print("* * * started recording data * * *")
         self._recording = True
-        start_time = time.time()
-        print(f"* * * recording has started at unix time {time.ctime(start_time)}. * * *\n")
+
+      elif self._recording and button_1_pressed:
+        self._write_csv_data()
+        self._data = []
+        self._id = 0
+        print("* * * stopped recording data * * *")
+        self._recording = False
+
 
       if self._recording:
-        data = self._input.get_state()
+        data = self._input.get_acceleration()
         self._process_sensor_data(data)
-
-        if start_time + self._duration < time.time():
-          self._recording = False
-          print(f"* * * recording has stopped at unix time {time.ctime(time.time())}. * * *\n")
-          self._input.terminate_sensor()
-          self._write_csv_data()
-          sys.exit()
 
       '''
         m5stack polling needs to be limited as sensor values do not change as often as python can call get_state. this would lead to a lot of dublicate data.
@@ -149,12 +159,10 @@ if __name__ == "__main__":
   parser = ArgumentParser(prog="Data Gatherer", description="this application gatheres gesture data from a M5Stack or an Android smartphone. data contains these values: id, gesture, acceleration(x,y,z) and timestamp. data will be saved to a csv file.")
   parser.add_argument("gesture", type=str, choices=list(Gesture.__members__), help=f"provide a gesture that you want to measure. gestures are: {list(Gesture.__members__)}.")
 
-  parser.add_argument("-d", "--duration", default=3, type=check_positive_int, help="[default: 10, unit: seconds] provide a duration in seconds that you want to measure your activity. the application automatically stops the recording and creates a csv file.")
   parser.add_argument("-pps", "--pollspersecond", default=50, type=check_positive_int, help="[default: 50, unit: per second] determine the frequency that the DIPPID device is polled for sensor data.")
-  parser.add_argument("-w", "--wait", default=3, type=check_positive_int, help="[default: 5, unit: seconds] when pressing button_1 to start recording, the application waits X second so that the user can put the device inside his pocket and get ready for the activity.")
   parser.add_argument("-f", "--file_location", default="data", type=str, help="[default: ./data] folder location where to save files.")
 
   args = parser.parse_args()
 
-  application = Application(Gesture[args.gesture.upper()], args.duration, args.pollspersecond, args.wait, args.file_location)
+  application = Application(Gesture[args.gesture.upper()], args.pollspersecond, args.file_location)
   application.run()
